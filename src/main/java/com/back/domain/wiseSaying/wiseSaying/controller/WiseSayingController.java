d12ef990bf53d1ac27f04f7daca527f395e1a380
zunobk <junho4560@naver.com>
2025-11-26 15:19:19 +0900

    명언 앱 구현 시작, 등록과 조회

---
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -0,0 +1,52 @@
+package com.back.domain.wiseSaying.wiseSaying.controller;
+
+import com.back.domain.wiseSaying.wiseSaying.entity.WiseSaying;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class WiseSayingController {
+    private int lastId = 0;
+    private final List<WiseSaying> wiseSayings = new ArrayList<>();
+
+    @GetMapping("/wiseSayings/write")
+    @ResponseBody
+    public String write(
+            String content,
+            String author
+    ) {
+        if (content == null || content.isBlank()) {
+            throw new IllegalArgumentException("Content cannot be null or blank");
+        }
+
+        if (author == null || author.isBlank()) {
+            throw new IllegalArgumentException("Author cannot be null or blank");
+        }
+
+        int id = ++lastId;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+
+        wiseSayings.add(wiseSaying);
+
+        return "%d번 명언이 생성되었습니다.".formatted(id);
+    }
+
+    @GetMapping("/wiseSayings")
+    @ResponseBody
+    public String list() {
+        return "<ul>"
+                + wiseSayings
+                .stream()
+                .map(wiseSaying ->
+                        "<li>%d / %s / %s</li>".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent())
+                )
+                .collect(Collectors.joining(""))
+                + "</ul>";
+    }
+}
---
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/entity/WiseSaying.java
@@ -0,0 +1,12 @@
+package com.back.domain.wiseSaying.wiseSaying.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+}
