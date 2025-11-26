d4b0f928d6dddbb8523833c3ef07bbeb0ac51130
zunobk <junho4560@naver.com>
2025-11-26 14:33:55 +0900

    Member 엔티티 추가, Member 샘플 데이터 5개 생성, user2 닉네임 변경

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,14 +1,20 @@
 package com.back;
 
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.member.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+    private final MemberService memberService;
+
     @Autowired
     @Lazy
     private AppConfig self;
@@ -19,28 +25,28 @@ public class AppConfig {
     }
 
     @Bean
-    ApplicationRunner myApplicationRunner() {
-        return new ApplicationRunner() {
-            @Override
-            public void run(ApplicationArguments args) throws Exception {
-                System.out.println("myApplicationRunner is running");
-            }
-        };
-    }
-
-    @Bean
-    ApplicationRunner myApplicationRunner2() {
+    ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
             self.work1();
             self.work2();
         };
     }
 
-    private void work1() {
-        System.out.println("work1");
+    @Transactional
+    public void work1() {
+        if (memberService.count() > 0) return;
+
+        Member memberSystem = memberService.join("system", "1234", "시스템");
+        Member memberAdmin = memberService.join("admin", "1234", "관리자");
+        Member memberUser1 = memberService.join("user1", "1234", "유저1");
+        Member memberUser2 = memberService.join("user2", "1234", "유저2");
+        Member memberUser3 = memberService.join("user3", "1234", "유저3");
     }
 
-    private void work2() {
-        System.out.println("work2");
+    @Transactional
+    public void work2() {
+        Member memberUser2 = memberService.findByUsername("user2").get();
+
+        memberUser2.setNickname("유저2 New");
     }
 }
