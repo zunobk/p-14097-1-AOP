5e9d56db98cb71658112ac6b923e4ecc7ab2bbf6
zunobk <junho4560@naver.com>
2025-11-26 11:26:58 +0900

    ApplicationRunner 는 @Bean 과 람다의 조합으로 만드는게 편하다.

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +10,31 @@ public class AppConfig {
     @Bean
     int version() {
         return 55;
+    }
+
+    @Bean
+    ApplicationRunner myApplicationRunner() {
+        return new ApplicationRunner() {
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                System.out.println("myApplicationRunner is running");
+            }
+        };
+    }
+
+    @Bean
+    ApplicationRunner myApplicationRunner2() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    private void work1() {
+        System.out.println("work1");
+    }
+
+    private void work2() {
+        System.out.println("work2");
     }
 }
