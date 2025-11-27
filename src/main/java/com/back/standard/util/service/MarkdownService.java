71de5da8863fdfa4cfeefc817310d2af2eff13a8
zunobk <junho4560@naver.com>
2025-11-27 14:43:28 +0900

    HtmlRenderer와 Parser은 공유자원이 아니기 때문에 빈에서 제거

--- src/main/java/com/back/standard/util/service/MarkdownService.java
+++ src/main/java/com/back/standard/util/service/MarkdownService.java
@@ -1,16 +1,19 @@
 package com.back.standard.util.service;
 
-import lombok.RequiredArgsConstructor;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class MarkdownService {
     private final HtmlRenderer htmlRenderer;
     private final Parser parser;
+
+    public MarkdownService() {
+        htmlRenderer = HtmlRenderer.builder().build();
+        parser = Parser.builder().build();
+    }
 
     public String toHtml(String markdown) {
         // 문자열을 파싱해서 Node 트리 구조로 변환
