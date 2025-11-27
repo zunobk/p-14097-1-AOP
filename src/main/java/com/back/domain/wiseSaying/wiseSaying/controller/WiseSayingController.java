ff541256aaad77a3f3b6627e0a1fa1d88fcb8b41
zunobk <junho4560@naver.com>
2025-11-27 10:43:56 +0900

    상세 페이지, 주소체계 변경(/wiseSayings/delete/{id} 에서 /wiseSayings/{id}/delete)

--- src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -59,7 +59,19 @@ public class WiseSayingController {
                 + "</ul>";
     }
 
-    @GetMapping("/wiseSayings/delete/{id}")
+    @GetMapping("/wiseSayings/{id}")
+    @ResponseBody
+    public String detail(@PathVariable int id) {
+        WiseSaying wiseSaying = findById(id).get();
+
+        return """
+                <h1>명언 : %s</h1>
+                <div>번호 : %d</div>
+                <div>작가 : %s</div>
+                """.formatted(wiseSaying.getContent(), wiseSaying.getId(), wiseSaying.getAuthor());
+    }
+
+    @GetMapping("/wiseSayings/{id}/delete")
     @ResponseBody
     public String delete(
             @PathVariable int id
@@ -74,7 +86,7 @@ public class WiseSayingController {
         return "%d번 명언이 삭제되었습니다.".formatted(id);
     }
 
-    @GetMapping("/wiseSayings/modify/{id}")
+    @GetMapping("/wiseSayings/{id}/modify")
     @ResponseBody
     public String modify(
             @PathVariable int id,
